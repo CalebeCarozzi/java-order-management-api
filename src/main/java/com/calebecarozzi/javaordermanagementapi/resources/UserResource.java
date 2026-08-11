efd6a2f7ca3e967bd8127f3ba0e888c87c570b10
Calebe Carozzi <calebercarozzi@outlook.com>
2026-08-11 00:53:04 -0300

    feat: add UserService and methods in it

--- src/main/java/com/calebecarozzi/javaordermanagementapi/resources/UserResource.java
+++ src/main/java/com/calebecarozzi/javaordermanagementapi/resources/UserResource.java
@@ -1,10 +1,15 @@
 package com.calebecarozzi.javaordermanagementapi.resources;
 
 import com.calebecarozzi.javaordermanagementapi.entities.User;
+import com.calebecarozzi.javaordermanagementapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 //pra fala que é um recurso web e que é implementado por um cntrolador web ->
 //dai depois precida de um nome
@@ -12,16 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/Users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     //response entity -> retorna respostas de requisição web
     //o getMapping pra fala que ele responde a uma requisição de tipo get
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        //apenas para testar
-        User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
 
-    
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
 
 
 }

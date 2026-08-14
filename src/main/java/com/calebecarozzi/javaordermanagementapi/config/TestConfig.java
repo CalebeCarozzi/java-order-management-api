2cfc896ed9a81f37ab8908db8d544651e2412bcb
Calebe Carozzi <calebercarozzi@outlook.com>
2026-08-14 19:33:56 -0300

    feat: add relation between Product and Ordem

--- src/main/java/com/calebecarozzi/javaordermanagementapi/config/TestConfig.java
+++ src/main/java/com/calebecarozzi/javaordermanagementapi/config/TestConfig.java
@@ -70,6 +70,7 @@ public class TestConfig implements CommandLineRunner {
         p4.getCategories().add(cat3);
         p5.getCategories().add(cat2);
 
+
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
         OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
--- src/main/java/com/calebecarozzi/javaordermanagementapi/entities/Product.java
+++ src/main/java/com/calebecarozzi/javaordermanagementapi/entities/Product.java
@@ -29,6 +29,10 @@ public class Product {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product() {
     }
 
@@ -82,6 +86,15 @@ public class Product {
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
